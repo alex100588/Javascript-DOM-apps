483055c90d81f85334c852979b7a7a21a4166ff3
alex100588 <49831245+alex100588@users.noreply.github.com>
2024-02-20 19:09:10 +0200

    Update script.js

--- Weather app/script.js
+++ Weather app/script.js
@@ -6,7 +6,6 @@ const searchBtn = document.querySelector('.search button')
 const weatherIcon = document.querySelector('weather-icon')
 
 
-const weather = document.querySelector('.weather').style.display = 'block';
 
 // function to check the weather in city using the api
 async function checkWeather(city){
@@ -16,14 +15,15 @@ async function checkWeather(city){
 		document.querySelector('.error').style.display = 'block';
 		document.querySelector('.weather').style.display = 'none';
 	} else {
-	let data = await response.json();
-	console.log(data);
-	
-	document.querySelector('.city').innerHTML = data.name;
-	document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
+		let data = await response.json();
+		console.log(data);
+		
+		document.querySelector('.city').innerHTML = data.name;
+		const temp = document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
 	document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
 	document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
-
+	
+	
 	if(data.weather.main == 'Clouds'){
 		weatherIcon.src = 'Images/clouds.png';
 	}
@@ -42,8 +42,9 @@ async function checkWeather(city){
 	else if(data.weather.main == 'Mist'){
 		weatherIcon.src = 'Images/mist.png';
 	}
-
-
+	
+	document.querySelector('.weather').style.display = 'block';
+	
 	}
 }
 
